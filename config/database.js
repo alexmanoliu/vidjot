42058718b2d6c3f4c0cf4169edc3a798d4f9e44b
Alex M <alexandru.manoliu@gmail.com>
2017-12-03 01:07:17 +0200

    some other changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://alxmnl88:vidjotapp@ds129156.mlab.com:29156/vidjotapp'}
+  module.exports = {mongoURI: 'mongodb://alexvid:vidjotapp@ds129156.mlab.com:29156/vidjotapp'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
