d908415dda8cf22ee8765dd9641d68338cb2a969
Alex M <alexandru.manoliu@gmail.com>
2017-12-03 01:58:46 +0200

    some changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds129156.mlab.com:29156/vidjot'}
+  module.exports = {mongoURI: 'mongodb://alexmnl:parola@ds129156.mlab.com:29156/vidjot'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
