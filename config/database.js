e0341ec5306ab3c4c7fa7d407afdaecc9d90c3fd
Alex M <alexandru.manoliu@gmail.com>
2017-12-03 01:52:12 +0200

    changed mongodb url

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI: 'mongodb://alexvid:vidjotapp@ds129156.mlab.com:29156/vidjotapp'}
+  module.exports = {mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds129156.mlab.com:29156/vidjot'}
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
